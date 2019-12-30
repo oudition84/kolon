fed8a0136fe53538ef833ce31b4a7905b12a3601
oudition84 <53218720+oudition84@users.noreply.github.com>
2019-12-30 14:39:01 +0900

    Add files via upload

---
+++ js/script.js
@@ -0,0 +1,32 @@
+$(document).ready(function(e){
+    
+//    //메인배너 페이드인,아웃
+//    $(".slideList").children("div:gt(0)").hide();
+//    var current = 0;
+//    
+//    setInterval(function(){
+//        var next = (current+1) % 3;
+//        $(".slideList").find("div").eq(current).fadeOut();
+//        $(".slideList").find("div").eq(next).fadeIn();
+//        current = next;
+//    },3000);
+//    
+//    
+
+    
+//    바로가기
+//    function onScroll(event){
+//		var scrollPos = $(document).scrollTop() + 200;
+//		$('#menu-center a').each(function () {
+//			var currLink = $(this);
+//			var refElement = $(currLink.attr('href'));
+//			if (refElement.offset().top < scrollPos && refElement.offset().top + refElement.height() > scrollPos) {
+//				$('#menu-center ul li a').removeClass('active');
+//				currLink.addClass('active');
+//			} else{
+//				currLink.removeClass('active');
+//			}
+//		});
+//	}
+    
+});
